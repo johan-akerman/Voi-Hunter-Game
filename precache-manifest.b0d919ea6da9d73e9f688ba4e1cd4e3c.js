c170a7ec897e1b9d7e7a3520158cab08a2893b4f
Johan <joha.aker@gmail.com>
2020-04-10 18:50:19 +0200

    Updates

---
+++ precache-manifest.b0d919ea6da9d73e9f688ba4e1cd4e3c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39f52542695e08905d8ca46af742f33",
+    "url": "/Voi-Hunter-Game/index.html"
+  },
+  {
+    "revision": "39bc02d531c9234fe17f",
+    "url": "/Voi-Hunter-Game/static/css/2.25d2dfdd.chunk.css"
+  },
+  {
+    "revision": "fa03fee9a8ab46116f97",
+    "url": "/Voi-Hunter-Game/static/css/main.ef930b11.chunk.css"
+  },
+  {
+    "revision": "39bc02d531c9234fe17f",
+    "url": "/Voi-Hunter-Game/static/js/2.b7a6ba16.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/Voi-Hunter-Game/static/js/2.b7a6ba16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa03fee9a8ab46116f97",
+    "url": "/Voi-Hunter-Game/static/js/main.6d0759c8.chunk.js"
+  },
+  {
+    "revision": "6ba66e3abb43d69da6ef",
+    "url": "/Voi-Hunter-Game/static/js/runtime-main.44a68ffc.js"
+  },
+  {
+    "revision": "268e5a664bf0ed8ed1fedea239770da7",
+    "url": "/Voi-Hunter-Game/static/media/click.268e5a66.mp3"
+  },
+  {
+    "revision": "69cf116dfe857f37b6690b3ddf2de040",
+    "url": "/Voi-Hunter-Game/static/media/map.69cf116d.png"
+  },
+  {
+    "revision": "bfe92c83765a78c9a961192a726c42be",
+    "url": "/Voi-Hunter-Game/static/media/voi-left.bfe92c83.png"
+  },
+  {
+    "revision": "924321d059142688db9461f09582b88c",
+    "url": "/Voi-Hunter-Game/static/media/voi-right.924321d0.png"
+  },
+  {
+    "revision": "68ba0c23d26d79175988811d4688367a",
+    "url": "/Voi-Hunter-Game/static/media/voi_logo_white.68ba0c23.svg"
+  },
+  {
+    "revision": "abff012e026b884b05fcc31c09a8f5fb",
+    "url": "/Voi-Hunter-Game/static/media/voila.abff012e.png"
+  },
+  {
+    "revision": "7b1299499febf14e41aaf4e2d40a1f03",
+    "url": "/Voi-Hunter-Game/static/media/walker.7b129949.png"
+  }
+]);
